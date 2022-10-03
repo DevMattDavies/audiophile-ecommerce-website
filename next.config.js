5868d9cb5bab98aae6d54d797b9fdcb2250e6893
Matt Davies <matt@mattdavies.dev>
2022-10-03 10:40:41 +0100

    initial project setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(ttf|woff|woff2|mp4|pdf|webm)$/,
+      type: "asset",
+      generator: {
+        filename: "static/chunks/[path][name].[hash][ext]",
+      },
+    });
+    return config;
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,45 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useState, useEffect, createContext } from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Set correct viewport height for different devices
+if (typeof window !== "undefined") {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  // We listen to the resize event
+  window.addEventListener("resize", () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
 }
 
-export default MyApp
+// Create width context with intial null value
+export const WidthContext = createContext(null);
+
+function MyApp({ Component, pageProps }) {
+  // Create state for device width
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize, false);
+  }, []);
+
+  return (
+    <WidthContext.Provider value={{ width }}>
+      <Component {...pageProps} width={width} />
+    </WidthContext.Provider>
+  );
+}
+
+export default MyApp;
